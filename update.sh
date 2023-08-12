ae58a2446ec9ef07a3bd878347b65a723986d76c
LAlto <lafon.ludovic.ll@proton.me>
2023-08-12 18:43:44 +0200

    Update flake.lock and update.sh
    - Backup flake.lock before updating
    - Remove content in /etc/nixos
    - Copy content from ~/Documents/nix-configuration to /etc/nixos
    - Remove update.sh from /etc/nixos
    - Remove flake.lock from ~/Documents/nix-configuration
    
    Update icon_map_path in parse_workspaces_hyprland.py
    
    - Change icon_map_path from '/home/ludoviclafon/.config/eww/scripts/workspaces_map.json' to '/etc/nixos/wm/eww/scripts/workspaces_map.json'

--- update.sh
+++ update.sh
@@ -1,3 +1,7 @@
+# Backup flake.lock
+rm /home/laptop/Documents/nix-configuration/flake.lock
+cp /etc/nixos/flake.lock /home/laptop/Documents/nix-configuration/flake.lock
+
 # Remove content in /etc/nixos
 rm -rf /etc/nixos/*
 echo "Removed content in /etc/nixos"
@@ -7,3 +11,5 @@ cp -r /home/laptop/Documents/nix-configuration/* /etc/nixos/
 # Remove update.sh from /etc/nixos
 rm /etc/nixos/update.sh
 echo "Copied content from ~/Documents/nix-configuration to /etc/nixos"
+
+rm /home/laptop/Documents/nix-configuration/flake.lock
