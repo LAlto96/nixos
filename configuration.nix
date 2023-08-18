0d4a07515567bda41486d4e17054dd4c35d3c608
Alto <lafon.ludovic.ll@proton.me>
2023-08-18 12:30:52 +0200

    Disable wlr.enable in xdg.portal configuration
    This commit disables the wlr.enable option in the xdg.portal configuration. This option is no longer needed and removing it will improve the performance of the system.

--- configuration.nix
+++ configuration.nix
@@ -126,7 +126,6 @@
   services.dbus.enable = true;
   xdg.portal = {
     enable = true;
-    wlr.enable = true;
     # gtk portal needed to make gtk apps happy
     extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   };
