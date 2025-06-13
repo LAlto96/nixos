16693a4ba2185d5dc2713e3d33c509c48c1b717f
Alto <lafon.ludovic.ll@proton.me>
2025-06-13 19:07:04 +0200

    Enable SDDM auto-login for user "desktop"
    - Uncommented the `autoLogin` settings in `flake.nix`.
    - Configured SDDM to automatically log in the user "desktop".
    - Useful for streamlined startup in a graphical session.

--- flake.nix
+++ flake.nix
@@ -169,8 +169,8 @@
             # };
             # services.xserver.enable = true;
             services.displayManager.sddm.enable = true;
-            # services.displayManager.autoLogin.enable = true;
-            # services.displayManager.autoLogin.user = "desktop";
+            services.displayManager.autoLogin.enable = true;
+            services.displayManager.autoLogin.user = "desktop";
             services.displayManager.sddm.wayland.enable = true;
             services.desktopManager.plasma6.enable = true;
             services.displayManager.defaultSession = "hyprland";
