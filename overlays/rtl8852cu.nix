cbeb514f5b80f61de334bf37040d28742ff6ed70
Ludovic Lafon <112064171+LAlto96@users.noreply.github.com>
2025-07-05 17:46:35 +0200

    Fix rtl8852cu overlay to extend kernel packages

--- overlays/rtl8852cu.nix
+++ overlays/rtl8852cu.nix
@@ -1,4 +1,17 @@
 self: super:
-{
-  rtl8852cu = super.callPackage ../packages/rtl8852cu.nix { };
+let
+  buildFor = kernelPackages:
+    kernelPackages.callPackage ../packages/rtl8852cu.nix {
+      inherit (kernelPackages) kernel;
+    };
+in {
+  rtl8852cu = buildFor self.linuxPackages;
+
+  linuxPackages = super.linuxPackages.extend (ks: _:
+    { rtl8852cu = buildFor ks; }
+  );
+
+  linuxPackages_zen = super.linuxPackages_zen.extend (ks: _:
+    { rtl8852cu = buildFor ks; }
+  );
 }
