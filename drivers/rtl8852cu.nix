43b5595a816e0bda0f3ac6214ed2f7ebef709367
Ludovic Lafon <112064171+LAlto96@users.noreply.github.com>
2025-07-05 17:40:52 +0200

    Add rtl8852cu WiFi driver overlay

---
+++ drivers/rtl8852cu.nix
@@ -0,0 +1,5 @@
+{ config, ... }:
+{
+  boot.kernelModules = [ "8852cu" ];
+  boot.extraModulePackages = with config.boot.kernelPackages; [ rtl8852cu ];
+}
---
+++ overlays/rtl8852cu.nix
@@ -0,0 +1,4 @@
+self: super:
+{
+  rtl8852cu = super.callPackage ../packages/rtl8852cu.nix { };
+}
