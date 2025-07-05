06e9cce533fe73a7ddacccbb8d59cce34130ae0e
Ludovic Lafon <112064171+LAlto96@users.noreply.github.com>
2025-07-05 16:30:15 +0200

    refactor rtl88x2bu overlay

--- rtl88x2bu-overlay.nix
+++ rtl88x2bu-overlay.nix
@@ -1,15 +1,17 @@
 self: super: {
-  rtl88x2bu = super.rtl88x2bu.overrideAttrs (old: {
-    src = super.fetchFromGitHub {
-      owner = "RinCat";
-      repo = "RTL88x2BU-Linux-Driver";
-      # commit with kernel >=6.15 patches
-      rev = "77a82dbac7192bb49fa87458561b0f2455cdc88f";
-      sha256 = "1qrhd4698808axm6mliq810s3yj8aj7nv890pdvpbir8nvn6c44h";
-    };
-    # ensure sources compile with kbuild
-    prePatch = (old.prePatch or "") + ''
-      sed -i 's|-I\\$(src)/include|-I\\$(PWD)/include|' Makefile
-    '';
+  linuxPackagesFor = kernel: let
+    base = super.linuxPackagesFor kernel;
+  in base.extend (final: prev: {
+    rtl88x2bu = prev.rtl88x2bu.overrideAttrs (old: {
+      src = super.fetchFromGitHub {
+        owner = "RinCat";
+        repo = "RTL88x2BU-Linux-Driver";
+        rev = "77a82dbac7192bb49fa87458561b0f2455cdc88f"; # commit with 6.15 patches
+        sha256 = "1qrhd4698808axm6mliq810s3yj8aj7nv890pdvpbir8nvn6c44h";
+      };
+      prePatch = (old.prePatch or "") + ''
+        sed -i 's|-I\\$(src)/include|-I\\$(PWD)/include|' Makefile
+      '';
+    });
   });
 }
