3951423a0117962503b91d8efcae79c84e7e2919
Ludovic Lafon <112064171+LAlto96@users.noreply.github.com>
2025-07-05 15:31:53 +0200

    Fix rtl88x2bu build on kernel 6.15

--- rtl88x2bu-overlay.nix
+++ rtl88x2bu-overlay.nix
@@ -3,8 +3,13 @@ self: super: {
     src = super.fetchFromGitHub {
       owner = "RinCat";
       repo = "RTL88x2BU-Linux-Driver";
-      rev = "master"; # or a specific tag compatible with kernel >=6.15
+      # commit with kernel >=6.15 patches
+      rev = "77a82dbac7192bb49fa87458561b0f2455cdc88f";
       sha256 = "1qrhd4698808axm6mliq810s3yj8aj7nv890pdvpbir8nvn6c44h";
     };
+    # ensure sources compile with kbuild
+    prePatch = (old.prePatch or "") + ''
+      substituteInPlace Makefile --replace "-I$(src)/include" "-I$(PWD)/include"
+    '';
   });
 }
