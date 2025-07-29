492472517062c1c3c3b3c3f502883f6c3621de6f
Alto <lafon.ludovic.ll@proton.me>
2025-07-29 20:44:17 +0200

    Enable GitHub Copilot in Doom Emacs config
    - Uncommented and activated the Copilot package via `use-package!` in `config.el`.
    - Set up keybindings to accept Copilot completions in `prog-mode`.
    - Uncommented the Copilot package declaration in `packages.el` to ensure it's installed.

--- hm/doom/packages.el
+++ hm/doom/packages.el
@@ -18,8 +18,8 @@
 ;; (package! another-package
 ;;   :recipe (:host github :repo "username/repo"))
 
-;;(package! copilot
-;;  :recipe (:host github :repo "copilot-emacs/copilot.el" :files ("*.el")))
+(package! copilot
+  :recipe (:host github :repo "copilot-emacs/copilot.el" :files ("*.el")))
 
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
